ac6e6e9a04aeee0274fc5ef5b3603486e64ff523
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-08-13 18:33:19 +0200

    add welford

---
+++ src/stroke/welford.h
@@ -0,0 +1,98 @@
+/*****************************************************************************
+ * Stroke
+ * Copyright (C) 2023 Adam Celarek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#pragma once
+
+#include "matrix.h"
+
+#include "cuda_compat.h"
+
+namespace stroke::welford {
+
+/// warning: without bessel correction. bessel is impossible according to https://stats.stackexchange.com/questions/47325/bias-correction-in-weighted-variance (see 2nd answer)
+template <int N_DIMS, typename scalar_t>
+struct WeightedMeanAndCov {
+    using vec_t = glm::vec<N_DIMS, scalar_t>;
+    using mat_t = glm::mat<N_DIMS, N_DIMS, scalar_t>;
+    scalar_t w_sum = 0;
+    vec_t v_mean = vec_t { 0 };
+    mat_t C = mat_t { 0 };
+
+    STROKE_DEVICES_INLINE
+    WeightedMeanAndCov() = default;
+
+    STROKE_DEVICES_INLINE
+    void addValue(scalar_t w, const vec_t& v)
+    {
+        w_sum += w;
+        if (w == scalar_t(0.0))
+            return;
+        const auto delta1 = (v - v_mean);
+        v_mean += scalar_t(w / w_sum) * delta1;
+        const auto delta2 = (v - v_mean);
+
+        C += w * glm::outerProduct(delta1, delta2);
+        assert(!isnan(C));
+    }
+
+    STROKE_DEVICES_INLINE
+    scalar_t weightSum() const
+    {
+        return w_sum;
+    }
+
+    STROKE_DEVICES_INLINE
+    vec_t mean() const
+    {
+        return v_mean;
+    }
+
+    STROKE_DEVICES_INLINE
+    mat_t cov_matrix() const
+    {
+        if (w_sum == scalar_t(0.0))
+            return mat_t(1);
+        return C / w_sum;
+    }
+};
+
+template <typename scalar_t, typename T>
+struct WeightedMean {
+    scalar_t w_sum = 0;
+    T v_mean = T {};
+
+    STROKE_DEVICES_INLINE
+    WeightedMean() = default;
+
+    STROKE_DEVICES_INLINE
+    void addValue(scalar_t w, const T& v)
+    {
+        w_sum += w;
+        if (w == scalar_t(0.0))
+            return;
+        const auto delta1 = v - v_mean;
+        v_mean += scalar_t(w / w_sum) * delta1; // this scalar_t cast makes the compiler happy, when compiling unit tests with autodiff.
+    }
+
+    STROKE_DEVICES_INLINE
+    const T& mean() const
+    {
+        return v_mean;
+    }
+};
+} // namespace stroke::welford
---
+++ unittests/welford_benchmark.cu
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ * Stroke
+ * Copyright (C) 2023 Adam Celarek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+#include "whack/kernel.h"
+#include "whack/random/generators.h"
+#include "whack/random/state.h"
+
+#include "stroke/matrix_functions.h"
+#include "stroke/welford.h"
+
+struct BenchmarkResults {
+    float mean;
+    float four_std_dev;
+};
+
+namespace {
+void run_matrix_benchmarks()
+{
+    constexpr auto n_welfords = 1'000'000;
+    constexpr auto n_samples = 1000;
+    constexpr auto n_threads_per_block = 128;
+    constexpr auto n_blocks = (n_welfords + n_threads_per_block - 1) / n_threads_per_block;
+    constexpr auto location = whack::Location::Device;
+
+    auto s = whack::random::make_state(location, n_welfords);
+    auto s_view = s.view();
+
+    whack::start_parallel(
+        location, n_blocks, n_threads_per_block, WHACK_KERNEL(=) {
+            WHACK_UNUSED(whack_gridDim);
+            WHACK_UNUSED(whack_blockDim);
+            WHACK_UNUSED(whack_threadIdx);
+            WHACK_UNUSED(whack_blockIdx);
+            const auto index = whack_blockIdx.x * whack_blockDim.x + whack_threadIdx.x;
+            if (index >= n_welfords)
+                return;
+
+            s_view(index) = whack::random::KernelGenerator(index, 0);
+        });
+
+    auto r = whack::make_tensor<float>(location, n_welfords);
+    auto r_view = r.view();
+
+    BENCHMARK("WeightedMeanAndCov<3, float>")
+    {
+        whack::start_parallel(
+            location, n_blocks, n_threads_per_block, WHACK_KERNEL(=) {
+                WHACK_UNUSED(whack_gridDim);
+                WHACK_UNUSED(whack_blockDim);
+                WHACK_UNUSED(whack_threadIdx);
+                WHACK_UNUSED(whack_blockIdx);
+                const auto index = whack_blockIdx.x * whack_blockDim.x + whack_threadIdx.x;
+                if (index >= n_welfords)
+                    return;
+
+                auto& rng = s_view(index);
+
+                stroke::welford::WeightedMeanAndCov<3, float> welford;
+                for (auto i = 0; i < n_samples; ++i) {
+                    const auto weight = rng.uniform() + 0.3f;
+                    welford.addValue(weight, rng.normal3());
+                }
+
+                const auto mean = welford.mean();
+                const auto cov = welford.cov_matrix();
+                r_view(index) = sum(mean) + sum(cov);
+            });
+    };
+
+    const auto r_host = r.host_copy();
+    const double sum = std::accumulate(r_host.host_vector().begin(), r_host.host_vector().end(), 0.0);
+    CHECK(sum / r_host.host_vector().size() == Catch::Approx(3).epsilon(0.1)); // large margin due to impossible bessel correction for reliability type weights
+}
+
+} // namespace
+
+TEST_CASE("welford benchmark")
+{
+    run_matrix_benchmarks();
+}
