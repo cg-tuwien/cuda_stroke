6e9c10e61987f839dc3cfe5d264623699178423e
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-10-04 17:36:14 +0200

    add utility.h, for now containing only swap(a, b)

---
+++ src/stroke/utility.h
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Stroke
+ * Copyright (C) 2023 Adam Celarek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#pragma once
+#include "cuda_compat.h"
+
+namespace stroke {
+template <typename T>
+STROKE_DEVICES_INLINE void swap(T& t1, T& t2) {
+	T tmp(t1);
+	t1 = t2;
+	t2 = tmp;
+}
+}
