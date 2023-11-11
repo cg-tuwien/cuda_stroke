402f7fbaeac0994a51bd2adedf5b73d5c176d5cd
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-11-11 13:23:09 +0100

    start implementing gradients. need to refactor now

---
+++ unittests/grad_matrix.cu
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Stroke
+ * Copyright (C) 2023 Adam Celarek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include "stroke/unittest/gradcheck.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "stroke/grad/matrix_functions.h"
+
+TEST_CASE("stroke linalg gradients")
+{
+    SECTION("dot")
+    {
+        const auto fun = [](const whack::Tensor<double, 1>& input) {
+            const auto [a, b] = stroke::extract<glm::dvec3, glm::dvec3>(input);
+            return stroke::pack_tensor<double>(dot(a, b));
+        };
+
+        const auto fun_grad = [](const whack::Tensor<double, 1>& input, const whack::Tensor<double, 1>& grad_output) {
+            const auto [a, b] = stroke::extract<glm::dvec3, glm::dvec3>(input);
+            const auto incoming_grad = stroke::extract<double>(grad_output);
+            const auto [grad_a, grad_b] = stroke::grad::dot(a, b, incoming_grad);
+            return stroke::pack_tensor<double>(grad_a, grad_b);
+        };
+
+        const auto test_data_host = whack::make_tensor<double>(whack::Location::Host, { 3, 2, 1, 2, 4, 3 }, 6);
+        stroke::check_gradient(fun, fun_grad, test_data_host, 0.0000001);
+    }
+}
