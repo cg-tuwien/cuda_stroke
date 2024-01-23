292917807de680c1725713b4f8a0e4d42be49d66
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-01-23 19:13:05 +0100

    implement gradient for quaternions

---
+++ src/stroke/grad/quaternions.h
@@ -0,0 +1,129 @@
+/****************************************************************************
+ *  Copyright (C) 2024 Adam Celarek (github.com/adam-ce, github.com/cg-tuwien)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ ****************************************************************************/
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+namespace stroke::grad {
+
+template <typename scalar_t>
+glm::qua<scalar_t> toMat3(const glm::qua<scalar_t>& quat, const glm::mat<3, 3, scalar_t>& incoming_grad)
+{
+    scalar_t qxx(quat.x * quat.x);
+    scalar_t qyy(quat.y * quat.y);
+    scalar_t qzz(quat.z * quat.z);
+    scalar_t qxz(quat.x * quat.z);
+    scalar_t qxy(quat.x * quat.y);
+    scalar_t qyz(quat.y * quat.z);
+    scalar_t qwx(quat.w * quat.x);
+    scalar_t qwy(quat.w * quat.y);
+    scalar_t qwz(quat.w * quat.z);
+
+    scalar_t grad_qxx = 0;
+    scalar_t grad_qyy = 0;
+    scalar_t grad_qzz = 0;
+    scalar_t grad_qxz = 0;
+    scalar_t grad_qxy = 0;
+    scalar_t grad_qyz = 0;
+    scalar_t grad_qwx = 0;
+    scalar_t grad_qwy = 0;
+    scalar_t grad_qwz = 0;
+
+    // mat<3, 3, T, Q> Result(T(1));
+    // Result[0][0] = T(1) - T(2) * (qyy +  qzz);
+    grad_qyy += incoming_grad[0][0] * -2;
+    grad_qzz += incoming_grad[0][0] * -2;
+
+    // Result[0][1] = T(2) * (qxy + qwz);
+    grad_qxy += incoming_grad[0][1] * 2;
+    grad_qwz += incoming_grad[0][1] * 2;
+
+    // Result[0][2] = T(2) * (qxz - qwy);
+    grad_qxz += incoming_grad[0][2] * 2;
+    grad_qwy += incoming_grad[0][2] * -2;
+
+    // Result[1][0] = T(2) * (qxy - qwz);
+    grad_qxy += incoming_grad[1][0] * 2;
+    grad_qwz += incoming_grad[1][0] * -2;
+
+    // Result[1][1] = T(1) - T(2) * (qxx +  qzz);
+    grad_qxx += incoming_grad[1][1] * -2;
+    grad_qzz += incoming_grad[1][1] * -2;
+
+    // Result[1][2] = T(2) * (qyz + qwx);
+    grad_qyz += incoming_grad[1][2] * 2;
+    grad_qwx += incoming_grad[1][2] * 2;
+
+    // Result[2][0] = T(2) * (qxz + qwy);
+    grad_qxz += incoming_grad[2][0] * 2;
+    grad_qwy += incoming_grad[2][0] * 2;
+
+    // Result[2][1] = T(2) * (qyz - qwx);
+    grad_qyz += incoming_grad[2][1] * 2;
+    grad_qwx += incoming_grad[2][1] * -2;
+
+    // Result[2][2] = T(1) - T(2) * (qxx +  qyy);
+    grad_qxx += incoming_grad[2][2] * -2;
+    grad_qyy += incoming_grad[2][2] * -2;
+    // return Result;
+
+    glm::qua<scalar_t> grad_quat = { 0, 0, 0, 0 };
+    // scalar_t qxx(quat.x * quat.x);
+    grad_quat.x += grad_qxx * quat.x * 2;
+
+    // scalar_t qyy(quat.y * quat.y);
+    grad_quat.y += grad_qyy * quat.y * 2;
+
+    // scalar_t qzz(quat.z * quat.z);
+    grad_quat.z += grad_qzz * quat.z * 2;
+
+    // scalar_t qxz(quat.x * quat.z);
+    grad_quat.x += grad_qxz * quat.z;
+    grad_quat.z += grad_qxz * quat.x;
+
+    // scalar_t qxy(quat.x * quat.y);
+    grad_quat.x += grad_qxy * quat.y;
+    grad_quat.y += grad_qxy * quat.x;
+
+    // scalar_t qyz(quat.y * quat.z);
+    grad_quat.y += grad_qyz * quat.z;
+    grad_quat.z += grad_qyz * quat.y;
+
+    // scalar_t qwx(quat.w * quat.x);
+    grad_quat.w += grad_qwx * quat.x;
+    grad_quat.x += grad_qwx * quat.w;
+
+    // scalar_t qwy(quat.w * quat.y);
+    grad_quat.w += grad_qwy * quat.y;
+    grad_quat.y += grad_qwy * quat.w;
+
+    // scalar_t qwz(quat.w * quat.z);
+    grad_quat.w += grad_qwz * quat.z;
+    grad_quat.z += grad_qwz * quat.w;
+
+    return grad_quat;
+}
+
+} // namespace stroke::grad
---
+++ unittests/grad_quaternions.cu
@@ -0,0 +1,52 @@
+/****************************************************************************
+ *  Copyright (C) 2024 Adam Celarek (github.com/adam-ce, github.com/cg-tuwien)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ ****************************************************************************/
+
+#include <stroke/unittest/gradcheck.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <stroke/grad/quaternions.h>
+#include <stroke/unittest/random_entity.h>
+#include <whack/random/generators.h>
+
+TEST_CASE("stroke quaternion gradients")
+{
+    using scalar_t = double;
+    using quat_t = glm::qua<scalar_t>;
+    using mat3_t = glm::mat<3, 3, scalar_t>;
+
+    const auto fun = [](const whack::Tensor<scalar_t, 1>& input) {
+        const auto quat = stroke::extract<quat_t>(input);
+        return stroke::pack_tensor<scalar_t>(glm::toMat3(quat));
+    };
+
+    const auto fun_grad = [](const whack::Tensor<scalar_t, 1>& input, const whack::Tensor<scalar_t, 1>& grad_output) {
+        const auto quat = stroke::extract<quat_t>(input);
+        const auto incoming_grad = stroke::extract<mat3_t>(grad_output);
+        const auto grad_quat = stroke::grad::toMat3(quat, incoming_grad);
+        return stroke::pack_tensor<scalar_t>(grad_quat);
+    };
+
+    whack::random::HostGenerator<scalar_t> rnd;
+    for (int i = 0; i < 10; ++i) {
+        stroke::check_gradient(fun, fun_grad, stroke::pack_tensor<scalar_t>(stroke::host_random_quaternion<scalar_t>(&rnd)), 0.0000001);
+    }
+}
